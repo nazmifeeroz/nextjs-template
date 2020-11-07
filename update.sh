939a7c12dcf02eb14e5f1d47d76721f3db65efc8
Nazmi <nazmifeeroz@hotmail.com>
2020-11-07 12:59:51 +0800

    :wrench: use update script

---
+++ update.sh
@@ -0,0 +1,4 @@
+yarn remove @xstate/react next react react-dom styled-components xstate
+yarn add @xstate/react next react react-dom styled-components xstate
+yarn remove @babel/core @babel/preset-env @babel/preset-react @storybook/eslint-config-storybook @storybook/react @testing-library/react babel-eslint babel-jest babel-loader babel-plugin-module-resolver eslint eslint-config-airbnb eslint-config-prettier eslint-import-resolver-babel-module eslint-plugin-import eslint-plugin-jest eslint-plugin-jsx-a11y eslint-plugin-react jest 
+yarn add --dev @babel/core @babel/preset-env @babel/preset-react @storybook/eslint-config-storybook @storybook/react @testing-library/react babel-eslint babel-jest babel-loader babel-plugin-module-resolver eslint eslint-config-airbnb eslint-config-prettier eslint-import-resolver-babel-module eslint-plugin-import eslint-plugin-jest eslint-plugin-jsx-a11y eslint-plugin-react jest
