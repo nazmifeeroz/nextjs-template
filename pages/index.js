17df00ffb851b353f005e9e50d25ec091f7ad417
Nazmi Feeroz <nazmifeeroz@hotmail.com>
2021-12-01 21:23:52 +0800

    :fire: remove storybook

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const IndexPage = () => (
   <Container>Next JS Template with Cloudfront AWS!</Container>
-)
+);
 
 const Container = styled.div`
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
-export default IndexPage
+export default IndexPage;
